058a71b1269786ff443ba2ed6c6f12683d808331
chandan-guptaa <chandanguptan0@gmail.com>
2025-05-06 16:20:10 +0530

    Add files via upload

---
+++ index.cjs
@@ -0,0 +1,47 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+
+app.use(cors());
+
+const NEWS_API_KEY = '66f4feeeb5eb465d99aed659ff041c5d'; 
+
+
+app.use(express.static(__dirname));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/news', async (req, res) => {
+    const { country = 'us', category = 'general' } = req.query;
+    const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${NEWS_API_KEY}`;
+    try {
+    const response = await axios.get(url);
+        
+        if (response.data.articles.length === 0) {
+            return res.json({
+                country,
+                category,
+                articles: []
+            });
+        }
+    const articles = response.data.articles.map(article => ({
+      title: article.title,
+      description: article.description,
+      url: article.url
+    }));
+
+        res.json({ country, category, articles });
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching news' });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
